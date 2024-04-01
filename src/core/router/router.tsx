621efd8d700017a773b9f5bcdc818fbc83474cb1
sakschmidt <sarah.schmidt@code.seat>
2024-04-01 13:42:04 +0200

    feat: add navigation to app

--- src/core/router/router.tsx
+++ src/core/router/router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { routes } from "./routes";
 import { DoggiesScene, KittiesScene } from "scenes";
+import { PropsWithChildren } from "react";
 
-export const Router: React.FC = () => {
+export const Router: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <BrowserRouter>
+      {children}
       <Routes>
         <Route path={routes.root} element={<KittiesScene />} />
         <Route path={routes.kitties} element={<KittiesScene />} />
