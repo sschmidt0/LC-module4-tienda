4fd7d99e9ceaf6788df885f2e3d99001f98a0335
sakschmidt <sarah.schmidt@code.seat>
2024-04-13 11:14:15 +0200

    feat: add clear all button

--- src/pods/cart/cart.component.tsx
+++ src/pods/cart/cart.component.tsx
@@ -1,16 +1,37 @@
 import * as React from "react";
 import cx from "classnames";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Button from "@mui/material/Button";
 import { SelectedItemContext } from "core/provider";
-import { CartItem } from "./components";
+import { CartItem, DeleteModal } from "./components";
 import styles from "./cart.module.scss";
+
+const LITERALS = {
+  clearCart: "Clear cart",
+  infoModalText: "Are you sure you want to delete all the items in the cart?",
+};
 
 export interface CartComponentProps {
   className?: string;
 }
 
 export const CartComponent = ({ className }: CartComponentProps) => {
-  const { selectedItems, onClick } = React.useContext(SelectedItemContext);
+  const { selectedItems, onClick, onClearAll } =
+    React.useContext(SelectedItemContext);
+  const [isOpenDeleteAllModal, setIsOpenDeleteAllModal] = React.useState(false);
+
+  const handleCloseDeleteAllClick = () => {
+    setIsOpenDeleteAllModal(true);
+  };
+
+  const handleCancel = () => {
+    setIsOpenDeleteAllModal(false);
+  };
+
+  const handleConfirmDeleteAll = () => {
+    onClearAll();
+    setIsOpenDeleteAllModal(false);
+  };
 
   return (
     <div className={cx(styles.container, className)}>
@@ -28,6 +49,21 @@ export const CartComponent = ({ className }: CartComponentProps) => {
             />
           ))}
         </div>
+      </div>
+      <div className={styles.container__button}>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleCloseDeleteAllClick}
+        >
+          {LITERALS.clearCart}
+        </Button>
+        <DeleteModal
+          isOpen={isOpenDeleteAllModal}
+          title={LITERALS.infoModalText}
+          onClose={handleCancel}
+          onDelete={handleConfirmDeleteAll}
+        />
       </div>
     </div>
   );
